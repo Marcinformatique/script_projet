994098644ac0612336073186eb7637122c334d76
Tristan PETIT <tristan.petit04@gmail.com>
2024-04-20 17:59:02 +0200

    modified:   Test.sh

--- Test.sh
+++ Test.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# ceci est un commentaire
-dg=$(ls -l)
-
-echo "$dg"
-
 if [[ -f "Notes.txt" ]];
     then
         echo "C'est bien un fichier"
     fi
+
+if [[ -d "Notes.txt" ]];
+    then 
+        echo "C'est un dossier !"
+    else
+        echo "Ce n'est pas un dossier !"
+    fi
+
+    #boucle for pour test sur des fichiers
+count=0
+for file in $(find ~)
+   do 
+    if [[ -f $file ]];
+        then
+        (( count++ ))
+        fi
+   done
+
+echo "Le nombre de fichier est de $count"
